d8308440ee61727e3da93a656c532213a8c40fef
João Pedro Martins Forte <jpforte2004@gmail.com>
2024-11-08 19:45:37 -0300

    feat(p2-pdm): cria flatlist e exibe dados fixos

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import {
+  FlatList,
+  Image,
   Pressable,
   StyleSheet, 
   Text, 
@@ -9,18 +11,47 @@ import {
 interface ImagemGato{
   id: string;
   url: string;
-  width: number;
-  height: number;
 }
 
 export default function App() {
-  const [listaDeGatos, setListaDeGatos] = useState<ImagemGato[]>([])
+  const [listaDeGatos, setListaDeGatos] = useState<ImagemGato[]>([
+    {
+      "id": "g6SI_ZZRh",
+      "url": "https://cdn2.thecatapi.com/images/g6SI_ZZRh.jpg"
+    },
+    {
+      "id": "19l",
+      "url": "https://cdn2.thecatapi.com/images/19l.gif"
+    },
+    {
+      "id": "d59",
+      "url": "https://cdn2.thecatapi.com/images/d59.jpg"
+    },
+    {
+      "id": "4bd",
+      "url": "https://cdn2.thecatapi.com/images/4bd.gif"
+    }
+  ])
 
   return (
     <View style={styles.container}>
       <Pressable style={styles.botao}>
         <Text style={styles.textoBotao}>Buscar Gatos</Text>
       </Pressable>
+      <FlatList
+        data={listaDeGatos}
+        renderItem={({item}) => (
+          <View style={styles.itemLista}>
+            <Image 
+              source={{uri: item.url}}
+              resizeMode='center'
+              style={styles.imagemItem}
+            />
+          </View>
+        )}
+        keyExtractor={item => item.id}
+        style={styles.lista}
+      />
     </View>
   );
 }
@@ -42,5 +73,19 @@ const styles = StyleSheet.create({
   textoBotao: {
     fontSize: 16,
     textAlign: 'center'
+  },
+  lista: {
+    width: '80%',
+    borderWidth: 2,
+    borderColor: 'black',
+    marginBottom: 20
+  },
+  itemLista: {
+    marginVertical: 10,
+    alignItems: 'center'
+  },
+  imagemItem: {
+    width: '70%',
+    height: 300
   }
 });
