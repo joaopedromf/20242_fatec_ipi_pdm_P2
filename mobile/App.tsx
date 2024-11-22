a993561a2d4db6b528a05cd513cb3b45513e19b1
João Pedro Martins Forte <jpforte2004@gmail.com>
2024-11-22 00:41:11 -0300

    style(p2-pdm): estiliza componente

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,18 @@
 import axios from 'axios';
+import { Ionicons } from '@expo/vector-icons';
 import { useState } from 'react';
 import {
+  ActivityIndicator,
   FlatList,
   Image,
+  ImageBackground,
   Pressable,
   StyleSheet, 
   Text, 
   View 
 } from 'react-native';
+
+const imagemFundo = require('./assets/imagem_fundo.png')
 
 interface ImagemGato{
   id: string;
@@ -16,43 +21,59 @@ interface ImagemGato{
 
 export default function App() {
   const [listaDeGatos, setListaDeGatos] = useState<ImagemGato[]>([])
+  const [carregando, setCarregando] = useState<boolean>(false)
   const [erro, setErro] = useState<boolean>(false)
 
   const buscarGatos = async () => {
     try{
       setErro(false)
+      setCarregando(true)
       const { data } = await axios.get("http://localhost:3000/api/gatos")
+      setCarregando(false)
       setListaDeGatos(data)
     }
     catch(e){
+      setCarregando(false)
       setErro(true)
     }
   }
 
   return (
     <View style={styles.container}>
-      <Pressable style={styles.botao} onPress={() => buscarGatos()}>
-        <Text style={styles.textoBotao}>Buscar Gatos</Text>
-      </Pressable>
-      {
-        erro ?
-          <Text>Erro</Text>
-        :
-          <FlatList
-            data={listaDeGatos}
-            renderItem={({item}) => (
-              <View style={styles.itemLista}>
-                <Image 
-                  source={{uri: item.url}}
-                  resizeMode='center'
-                  style={styles.imagemItem}
-                />
+      <ImageBackground source={imagemFundo} style={styles.imagemFundo}>
+        <Text style={styles.titulo}>Buscador de Gatos <Ionicons name="logo-octocat" size={35} style={styles.iconeGato}></Ionicons></Text>
+        <Pressable style={styles.botao} onPress={() => buscarGatos()}>
+          <Text style={styles.textoBotao}>Buscar</Text>
+        </Pressable>
+        {
+          carregando && !erro ? 
+            <ActivityIndicator size='large' color='black' />
+          :
+            erro ?
+              <View style={styles.containerErro}>
+                <Ionicons name="close-circle-outline" size={40} color='darkred'></Ionicons>
+                <Text style={styles.textoErro}>Erro ao buscar as imagens.</Text>
               </View>
-            )}
-            keyExtractor={item => item.id}
-            style={styles.lista}
-          />
-      }
+            :
+              listaDeGatos.length > 0 ?
+                <FlatList
+                  data={listaDeGatos}
+                  renderItem={({item}) => (
+                    <View style={styles.itemLista}>
+                      <Image 
+                        source={{uri: item.url}}
+                        resizeMode='cover'
+                        style={styles.imagemItem}
+                      />
+                    </View>
+                  )}
+                  keyExtractor={item => item.id}
+                  style={styles.lista}
+                />
+              :
+              ""
+        }
+      </ImageBackground>
     </View>
   );
 }
@@ -60,16 +81,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#fff'
+  },
+  imagemFundo: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%'
+  },
+  titulo: {
+    fontSize: 32,
+    textAlign: 'center',
+    marginVertical: 20,
+    marginHorizontal: 5,
+    paddingVertical: 16,
+    paddingHorizontal: 10,
+    backgroundColor: 'white',
+    borderRadius: 8
+  },
+  iconeGato: {
+    verticalAlign: 'bottom',
+    marginLeft: 10
   },
   botao: {
     width: '80%',
-    backgroundColor: 'cyan',
+    backgroundColor: 'blanchedalmond',
     padding: 10,
     borderRadius: 8,
-    marginVertical: 20
+    marginBottom: 20,
+    borderWidth: 1
   },
   textoBotao: {
     fontSize: 16,
@@ -77,8 +117,10 @@ const styles = StyleSheet.create({
   },
   lista: {
     width: '80%',
-    borderWidth: 2,
+    backgroundColor: 'rgba(255,255,255,0.5)',
+    borderWidth: 1,
     borderColor: 'black',
+    borderRadius: 8,
     marginBottom: 20
   },
   itemLista: {
@@ -86,7 +128,21 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   imagemItem: {
-    width: '70%',
-    height: 300
+    width: 400,
+    height: 400
+  },
+  containerErro: {
+    backgroundColor: 'white',
+    borderWidth: 3,
+    borderColor: 'darkred',
+    paddingHorizontal: 15,
+    paddingVertical: 30,
+    borderRadius: 8,
+    alignItems: 'center'
+  },
+  textoErro: {
+    fontSize: 20,
+    fontWeight: 600,
+    color: 'darkred'
   }
 });
